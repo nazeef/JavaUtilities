8f06d08fc16183f9f2588542a8ac9490cadb6642
nazeef <feezan169@gmail.com>
2017-03-21 14:33:14 +0530

    Watson ajax

---
+++ JavaUtilities/watson.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $("button").click(function(){
+        $.ajax({
+			url: "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19",
+			beforeSend: function(xhr) { 
+			  xhr.setRequestHeader("Authorization", "Basic " + btoa("c77c2dbe-605b-4ab4-ab58-0b65b95809aa:Gsod3oRjaWDN")); 
+			},
+			type: 'POST',
+			dataType: 'json',
+			contentType: 'application/json',
+			processData: false,
+			data: '{"text": "A word is dead when it is said, some say. Emily Dickinson"}',
+			success: function (data) {
+			  alert(JSON.stringify(data));
+			},
+			error: function(){
+			  alert("Cannot get data");
+			}
+		});
+
+    });
+});
+</script>
+</head>
+<body>
+
+<div id="div1"><h2>Let jQuery AJAX Change This Text</h2></div>
+
+<button>Get External Content</button>
+
+</body>
+</html>
